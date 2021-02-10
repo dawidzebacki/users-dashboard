06495e3ec2cb9d0446ba4639fcfb16f761dd4568
Dawid Zebacki <dawidzebacki@protonmail.com>
2021-02-11 00:11:26 +0100

    add: API methods

---
+++ src/api/apiHandler.js
@@ -0,0 +1,92 @@
+const fetchAllUsers = async () => {
+    try {
+        const fetchResponse = await fetch(`${process.env.REACT_APP_API_ENDPOINT}`);
+
+        if (fetchResponse.ok === true) {
+            const data = await fetchResponse.json();
+            console.log('hi')
+            return data;
+        }
+
+        return fetchResponse;
+
+    } catch (e) {
+        return e;
+    }
+}
+
+const addUserAPI = async (user) => {
+    const settings = {
+        method: 'POST',
+        body: JSON.stringify(user),
+        headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+        }
+    };
+
+    try {
+        const fetchResponse = await fetch(`${process.env.REACT_APP_API_ENDPOINT}`, settings);
+
+        if (fetchResponse.ok === true) {
+            const data = await fetchResponse.json();
+            return data;
+        }
+
+        return fetchResponse;
+
+    } catch (e) {
+        return e;
+    }
+}
+
+const editUserAPI = async (user) => {
+    const settings = {
+        method: 'PUT',
+        body: JSON.stringify(user),
+        headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+        }
+    };
+
+    try {
+        const fetchResponse = await fetch(`${`${process.env.REACT_APP_API_ENDPOINT}`}/${user.id}`, settings);
+
+        if (fetchResponse.ok === true) {
+            const data = await fetchResponse.json();
+            return data;
+        }
+
+        return fetchResponse;
+
+    } catch (e) {
+        return e;
+    }
+}
+
+const deleteUserAPI = async (user) => {
+    const settings = {
+        method: 'DELETE',
+        headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+        }
+    };
+
+    try {
+        const fetchResponse = await fetch(`${`${process.env.REACT_APP_API_ENDPOINT}`}/${user.id}`, settings);
+
+        if (fetchResponse.ok === true) {
+            const data = await fetchResponse.json();
+            return data;
+        }
+
+        return fetchResponse;
+
+    } catch (e) {
+        return e;
+    }
+}
+
+export { fetchAllUsers, addUserAPI, editUserAPI, deleteUserAPI };
