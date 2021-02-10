ee4ccfbea72561aebb287a8ca41f19a94c40fe45
Dawid Zebacki <dawidzebacki@protonmail.com>
2021-02-11 00:15:27 +0100

    add: store provider to app

--- src/index.js
+++ src/index.js
@@ -2,10 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import store from './store/store';
+import { Provider } from 'react-redux';
+import 'antd/dist/antd.css'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
