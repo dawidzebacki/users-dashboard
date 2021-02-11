32e6aec7f64a4790b124b654d97013cfebe80fdd
Dawid Zebacki <dawidzebacki@protonmail.com>
2021-02-11 03:00:08 +0100

    change: API_Error component to show notifications

--- src/components/API_Error/API_Error.js
+++ src/components/API_Error/API_Error.js
@@ -1,24 +1,42 @@
-import React, { useState, useEffect } from 'react';
-import { useSelector, } from 'react-redux';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import {
     selectApiResponse,
 } from '../../store/usersSlice';
+import { notification } from 'antd';
+import { WarningOutlined } from '@ant-design/icons';
+
+import { apiError } from '../../store/usersSlice';
 
 const ApiError = () => {
 
+    const dispatch = useDispatch();
     const API = useSelector(selectApiResponse);
 
-    const [err, setErr] = useState(false);
+    const openNotification = () => {
+        notification.open({
+            message: 'API Issue',
+            duration: 5,
+            description:
+                'API responded with an incorrect message, the expected action could not be performed.',
+            icon: <WarningOutlined style={{ color: '#ffa940' }} />,
+        });
+    };
 
     useEffect(() => {
         if (API.status !== 200) {
-            setErr(true);
+            openNotification();
+            const error = {
+                statusText: "",
+                status: 200
+            }
+            dispatch(apiError(error))
         }
+        // eslint-disable-next-line
     }, [API]);
 
     return (
         <>
-            {err && <p>error api</p>}
         </>
     )
 }
